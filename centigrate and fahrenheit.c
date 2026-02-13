d6d7d0ca95f23eefe71fc248f0e9f469fe2470eb
Shivam18012007 <25116cn388@glbitm.ac.in>
2026-02-13 12:26:23 +0530

    Add files via upload

---
+++ centigrate and fahrenheit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float c,f;
+    printf("enter centigrade=");
+    scanf("%f",&c);
+
+    f=(9*c)/5+32;
+    printf("farenheit= %f",f);
+
+
+}
