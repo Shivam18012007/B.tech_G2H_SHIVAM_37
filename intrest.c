19eb180e30c105f86f1028efe0b928770a352ce4
Shivam18012007 <25116cn388@glbitm.ac.in>
2026-02-13 11:25:03 +0530

    Add files via upload

---
+++ intrest.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float p,r,t,si,ci,a;
+    printf("enter principle:");
+    scanf("%f",&p);
+    printf("enter the rate of intrest");
+    scanf("%f",&r);
+    printf("enter the time period:");
+    scanf("%f",&t);
+
+    si=(p*r*t)/100 ;
+    printf("the si of thr given principle amount will be:%f",si);
+    a=p*(pow((1+r/100),t));
+    ci=a-p
+}
